e93a4d667f3c4e9f611ae6f583311cb785a346cd
thomas <thomas@computoms.com>
2025-08-25 07:23:43 +0200

    Add dates in detail reports

--- clocknet/Reports/DetailsReport.cs
+++ clocknet/Reports/DetailsReport.cs
@@ -16,7 +16,9 @@ public class DetailsReport : IReport
     {
         _display.Print(
             activities
-                .SelectMany(LayoutActivity)
+                .Select(a => new {Date = a.Records.OrderBy(r => r.StartTime.Date).FirstOrDefault()?.StartTime.Date, Activity = a})
+                .GroupBy(x => x.Date)
+                .SelectMany(g => LayoutActivitiesOfTheDay(g.Key, g.Select(x => x.Activity)))
                 .Append(" ")
                 .Append(TotalTime(activities)));
     }
@@ -24,17 +26,22 @@ public class DetailsReport : IReport
     private string TotalTime(IEnumerable<Activity> activities)
         => _display.Layout($"{Utilities.PrintDuration(activities.Aggregate<Activity, TimeSpan>(TimeSpan.Zero, (curr, act) => curr + act.Duration))} Total");
 
+    private IEnumerable<string> LayoutActivitiesOfTheDay(DateTime? date, IEnumerable<Activity> activities)
+    {
+        return activities.SelectMany(LayoutActivity).Prepend(date?.ToString("yyyy-MM-dd") ?? "");
+    }
+
     private IEnumerable<string> LayoutActivity(Activity activity)
     {
         var duration = Utilities.PrintDuration(activity.Duration);
-	    var tags = string.Join(' ', activity.Task.Tags.Select(x => $"+{x}"));
+        var tags = string.Join(' ', activity.Task.Tags.Select(x => $"+{x}"));
         var id = string.IsNullOrWhiteSpace(activity.Task.Id) ? "" : $".{activity.Task.Id}";
         var line = $"{duration}"
             + activity.Task.Title.PrependSpaceIfNotNull()
             + tags.PrependSpaceIfNotNull()
             + id.PrependSpaceIfNotNull();
 
-        return activity.Records.Select(LayoutRecords).Prepend(_display.Layout(line.Trim()));
+        return activity.Records.Select(LayoutRecords).Prepend(_display.Layout(line.Trim(), 1));
     }
 
     private string LayoutRecords(Record record)
